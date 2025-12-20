0be69b5d28ad4c08426898baa8325518aa32a2d9
Micg25 <gglmhl02l25c351x@studium.unict.it>
2025-12-20 18:22:00 +0100

    packages added

---
+++ Spotify History Analyzer/common/StreamRecord.java
@@ -0,0 +1,29 @@
+package common;
+
+import java.io.Serializable;
+
+public class StreamRecord implements Serializable{
+    public String ts;
+    public String platform;
+    public long ms_played;
+    public String conn_country;
+    public String ip_addr;
+    public String master_metadata_track_name;
+    public String master_metadata_album_artist_name;
+    public String master_metadata_album_album_name;
+    public String spotify_track_uri;
+    public String episode_name;
+    public String episode_show_name;
+    public String audiobook_title;
+    public String audiobook_uri;
+    public String audiobook_chapter_uri;
+    public String audiobook_chapter_title;
+    public String reason_start;
+    public String reason_end;
+    public Boolean shuffle;
+    public Boolean skipped;
+    public Boolean offline;
+    public String offline_timestamp;
+    public Boolean incognito_mode;
+    
+}
