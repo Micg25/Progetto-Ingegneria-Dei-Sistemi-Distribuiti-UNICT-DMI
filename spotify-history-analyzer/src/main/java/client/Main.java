0fe6c6d36b09498b82189948f502b17bf51a904d
Micg25 <gglmhl02l25c351x@studium.unict.it>
2025-12-21 15:57:22 +0100

    updating main

--- spotify-history-analyzer/src/main/java/client/Main.java
+++ spotify-history-analyzer/src/main/java/client/Main.java
@@ -3,6 +3,7 @@ package client;
 import java.io.File; 
 import java.io.FileReader;
 import java.io.Reader;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
 
@@ -10,29 +11,28 @@ import common.StreamRecord;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("1. Inizio il test di Gson...");
 
         // Proviamo a creare l'oggetto Gson. Se la libreria manca, qui esploderà.
-        try {
-            Gson gson = new Gson();
-            System.out.println("2. Oggetto Gson creato con successo! La libreria è presente.");
-        } catch (NoClassDefFoundError e) {
-            System.out.println("ERRORE FATALE: La libreria Gson non è trovata!");
-            return;
-        }
+
+        Gson gson = new Gson();
+        System.out.println("2. Oggetto Gson creato con successo! La libreria è presente.");
 
         // Test di lettura file
-        String filePath = "Spotify Extended Streaming History/Streaming_History_Audio_2017-2020_0.json";
+        System.out.println("Di quale hanno vuoi vedere quanti ascolti hai?");
+        int anno = input.nextInt();
+    
+        String filePath = "Spotify Extended Streaming History/Streaming_History_Audio_%d-2020_0.json".formatted(anno);
         
         File f = new File(filePath);
         if (f.exists()) {
              System.out.println("3. File JSON trovato: " + f.getAbsolutePath());
              try (Reader reader = new FileReader(filePath)) {
-                 Gson gson = new Gson();
                  StreamRecord[] records = gson.fromJson(reader, StreamRecord[].class);
                  System.out.println("4. LETTURA RIUSCITA! Trovati " + records.length + " record.");
                  if(records.length > 0) {
-                     System.out.println("   Primo brano: " + records[0].toString());
+                     System.out.println(" Primo brano: " + records[0].toString());
                  }
              } catch (Exception e) {
                  e.printStackTrace();
