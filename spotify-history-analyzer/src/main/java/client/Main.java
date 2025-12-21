3708bb57acf298b3b2af105dc2684809a0018a8b
Micg25 <gglmhl02l25c351x@studium.unict.it>
2025-12-21 16:20:43 +0100

    updated main

--- spotify-history-analyzer/src/main/java/client/Main.java
+++ spotify-history-analyzer/src/main/java/client/Main.java
@@ -1,13 +1,11 @@
 package client;
 
-import java.io.File; 
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
-
-import common.StreamRecord;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,25 +18,47 @@ public class Main {
         System.out.println("2. Oggetto Gson creato con successo! La libreria è presente.");
 
         // Test di lettura file
-        System.out.println("Di quale hanno vuoi vedere quanti ascolti hai?");
+        System.out.println("Di quale anno vuoi vedere quanti ascolti hai?");
         int anno = input.nextInt();
-    
-        String filePath = "Spotify Extended Streaming History/Streaming_History_Audio_%d-2020_0.json".formatted(anno);
         
-        File f = new File(filePath);
-        if (f.exists()) {
-             System.out.println("3. File JSON trovato: " + f.getAbsolutePath());
-             try (Reader reader = new FileReader(filePath)) {
-                 StreamRecord[] records = gson.fromJson(reader, StreamRecord[].class);
-                 System.out.println("4. LETTURA RIUSCITA! Trovati " + records.length + " record.");
-                 if(records.length > 0) {
-                     System.out.println(" Primo brano: " + records[0].toString());
-                 }
-             } catch (Exception e) {
-                 e.printStackTrace();
-             }
-        } else {
-             System.out.println("ATTENZIONE: Non trovo il file JSON, ma se vedi il punto 2, Gson funziona comunque!");
+        String folderPath = "Spotify Extended Streaming History";
+        File folder = new File (folderPath);
+        if(!folder.exists() || !folder.isDirectory()){
+            System.out.println("Errore: Cartella non trovata: " + folder.getAbsolutePath());
+            return;
         }
+        File[] listOfFiles = folder.listFiles();
+        List<String> targetFiles = new ArrayList<>();
+        int totalSongs=0;
+        System.out.println("Sono stati trovati "+listOfFiles.length+" file");
+        if(listOfFiles != null){
+            for (int i=0; i<listOfFiles.length; i++){     
+                if(listOfFiles[i].getName().startsWith("Streaming_History_Audio_") && listOfFiles[i].getName().contains(String.valueOf(anno))){
+                    targetFiles.add(listOfFiles[i].getName());
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+        System.out.println(targetFiles);
+        
+        //String filePath = "Spotify Extended Streaming History/Streaming_History_Audio_%d-2020_0.json".formatted(anno);
+        //
+        //File f = new File(filePath);
+        //if (f.exists()) {
+        //     System.out.println("3. File JSON trovato: " + f.getAbsolutePath());
+        //     try (Reader reader = new FileReader(filePath)) {
+        //         StreamRecord[] records = gson.fromJson(reader, StreamRecord[].class);
+        //         System.out.println("4. LETTURA RIUSCITA! Trovati " + records.length + " record.");
+        //         if(records.length > 0) {
+        //             System.out.println(" Primo brano: " + records[0].toString());
+        //         }
+        //     } catch (Exception e) {
+        //         e.printStackTrace();
+        //     }
+        //} else {
+        //     System.out.println("ATTENZIONE: Non trovo il file JSON, ma se vedi il punto 2, Gson funziona comunque!");
+        //}
     }
 }
